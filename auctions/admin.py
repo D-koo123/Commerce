ab64cad24f79b9c8cd9b68390934471df16221ff
David Koo <nyanganka@gmail.com>
2024-10-15 19:43:43 +0300

    Added the models to the admin page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Listing, User, Bids_table, Comments, Watchlist
 
 # Register your models here.
+admin.site.register(Listing)
+admin.site.register(Bids_table)
+admin.site.register(Watchlist)
+admin.site.register(Comments)
+
